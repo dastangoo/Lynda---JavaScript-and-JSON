706c203bdfcc2a03e1c25d0d88cfeb0b9a7ea1f2
dastangoo <hamed.dastangoo@gmail.com>
2017-08-04 12:10:19 +0430

    JSONP

--- myscript.js
+++ myscript.js
@@ -1,17 +1,5 @@
-var request;
-if (window.XMLHttpRequest) {
-	request = new XMLHttpRequest();
-} else {
-	request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-request.open('GET', 'data.json');
-request.onreadystatechange = function () {
-	if ((request.status === 200) && (request.readyState === 4)) {
-		
-		var info = JSON.parse(request.responseType);
-		
-		var output = '';
+function dataHandler(info) {
+	var output = '';
 		for (var i = 0; i <= info.links.length; i++) {
 			for (key in info.links[i]) {
 				if(info.links[i].hasOwnProperty(key)) {
@@ -24,8 +12,4 @@ request.onreadystatechange = function () {
 		}
 		var update = document.getElementById('links');
 		update.innerHTML = output;
-		
-	} // ready
-} // event
-
-request.send();
+}
